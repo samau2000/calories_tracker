13fe575a521a80a63abbf5ea018a3d871883a2b5
sam <32819680+samau2000@users.noreply.github.com>
2021-09-12 19:21:00 -0400

    update recipes and add ios app to firebase

---
+++ test_app/lib/pages/recipes.dart
@@ -0,0 +1,106 @@
+//import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+/*
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blue,
+          accentColor: Colors.orange),
+      home: MyApp(),
+    ));
+*/
+
+class listRecipe extends StatefulWidget {
+  const listRecipe({Key? key, required User user})
+      : _user = user,
+        super(key: key);
+
+  final User _user;
+  @override
+  _listRecipeState createState() => _listRecipeState();
+}
+
+class _listRecipeState extends State<listRecipe> {
+  late User _user;
+  List recipe = <String>[];
+  String input = "";
+
+  // createRecipe() {
+  //   DocumentReference documentReference =
+  //       Firestore.instance.collection("MyRecipes").document(input);
+
+  //   Map<String, String> recipes = {"recipesTitle": input};
+
+  //   documentReference.setData(recipes).whenComplete(() {
+  //     print("$input created");
+  //   });
+  // }
+
+  // deleteRecipe() {}
+
+  @override
+  void initState() {
+    recipe.add("Salad");
+    recipe.add("Burger");
+    recipe.add("Fries");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Recipes"),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("Add Recipe"),
+                    content: TextField(onChanged: (String value) {
+                      input = value;
+                    }),
+                    actions: <Widget>[
+                      TextButton(
+                          onPressed: () {
+                            setState(() {
+                              recipe.add(input);
+                            });
+                          },
+                          child: Text("Add"))
+                    ],
+                  );
+                });
+          },
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          )),
+      body: ListView.builder(
+          itemCount: recipe.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+                key: Key(recipe[index]),
+                child: Card(
+                  child: ListTile(
+                      title: Text(recipe[index]),
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            recipe.removeAt(index);
+                          });
+                        },
+                      )),
+                ));
+          }),
+    );
+  }
+}
